8efaab3b9e8331fe649a5e7a51b1b0d93ce01fee
Grégoire Aubert <gregoire.aubert@heig-vd.ch>
2012-11-14 15:33:30 +0100

    changement de classe de base

--- src/com/cinemania/client/main.java
+++ src/com/cinemania/client/main.java
@@ -1,36 +1,78 @@
 package com.cinemania.client;
 
+import org.andengine.engine.Engine;
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
-import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.ui.activity.BaseGameActivity;
 
-public class main extends SimpleBaseGameActivity {
+public class main extends BaseGameActivity {
+	
+	// ===========================================================
+    // Constants
+    // ===========================================================
 
-	private Camera camera;
 	private static final int CAMERA_WIDTH = 800;
 	private static final int CAMERA_HEIGHT = 480;
 	
+	// ===========================================================
+    // Fields
+    // ===========================================================
+
+	private Camera mCamera;
+	private Scene mMainScene;
+	
+	// ===========================================================
+    // Constructors
+    // ===========================================================
+
+    // ===========================================================
+    // Getter & Setter
+    // ===========================================================
+
+    // ===========================================================
+    // Methods for/from SuperClass/Interfaces
+    // ===========================================================
+	
 	@Override
 	public EngineOptions onCreateEngineOptions() {
-		camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-        EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
+		mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);
         return engineOptions;
 	}
 
 	@Override
-	protected void onCreateResources() {
-		// TODO Auto-generated method stub
+	public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback)	throws Exception {
+		pOnCreateResourcesCallback.onCreateResourcesFinished();
 	}
 
 	@Override
-	protected Scene onCreateScene() {
-		Scene scene = new Scene();
-        scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
-        return scene;
+	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)	throws Exception {
+		
+		mEngine.registerUpdateHandler(new FPSLogger());
+	    mMainScene = new Scene();
+	    mMainScene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
+	    pOnCreateSceneCallback.onCreateSceneFinished(mMainScene);
 	}
+
+	@Override
+	public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+		pOnPopulateSceneCallback.onPopulateSceneFinished();
+	}
+	
+	// ===========================================================
+    // Methods
+    // ===========================================================
+
+
+	
+	
+    // ===========================================================
+    // Inner and Anonymous Classes
+    // ===========================================================
 
 }
