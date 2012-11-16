0aa5fb565d11139150bdd0d177482a048aedccda
Joel C. <godsend_l@yahoo.fr>
2012-11-16 19:44:04 +0100

    All classes which produces profit implements the Interface Profitable. All the profits are now calculated. However we can improve the profit of a movie according to the resources. To be continued...

---
+++ src/com/cinemania/client/Profitable.java
@@ -0,0 +1,17 @@
+package com.cinemania.client;
+
+/**
+ * Interface for all case which offer resources. 
+ * @author joel
+ *
+ */
+public interface Profitable {
+	
+	/**
+	 * Return the resource according to the kind of building. The compute is the actual depend on
+	 * the level, number of extensions, the start and stop turn.
+	 * We need to know the start turn and the stop turn. When a player turn all over the boardgame, 
+	 * he/she could get the profit of its cases
+	 */
+	public int profit(int startTurn, int stopTurn);
+}
