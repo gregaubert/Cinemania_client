23a3f6f5b2d7750a51f671dc15fa3c09e0f39945
Jorge Albaladejo <jorge@jorge-x60>
2012-11-16 13:13:00 +0100

    missing board

---
+++ src/com/cinemania/client/Board.java
@@ -0,0 +1,33 @@
+package com.cinemania.client;
+
+import org.andengine.entity.sprite.Sprite;
+
+public class Board implements Constantes {
+
+	private Case[] mCases = new Case[Constantes.BOARD_SIZE];
+
+	private int size = Constantes.BOARD_SIZE;
+
+	public Board() {
+		populate();
+	}
+
+	private void populate() {
+		for (int i = 0; i < size; i++) {
+			mCases[i] = new Cinema();
+		}
+	}
+	
+	public Case getCaseAtIndex(int index){
+		try{
+			return mCases[index];
+		}
+		catch (IndexOutOfBoundsException e){
+			return null;
+		}		
+	}
+	
+	public Case[] getCases(){
+		return mCases;
+	}
+}
