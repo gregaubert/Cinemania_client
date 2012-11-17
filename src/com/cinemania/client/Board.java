beb04b230e8ba1a2ed07e8881069c001e461d388
Gregoire Aubert <gregoire.aubert@heig-vd.ch>
2012-11-17 13:57:18 +0100

    Debut de la génération aléatoire du board, commit pour pouvoir bosser sur le fix

--- src/com/cinemania/client/Board.java
+++ src/com/cinemania/client/Board.java
@@ -5,6 +5,14 @@ public class Board implements Constantes {
 	private Case[] mCases = new Case[Constantes.BOARD_SIZE];
 
 	private int size = Constantes.BOARD_SIZE;
+	
+	private int currentHQ = 0;
+	private int currentScript;
+	private int currentCinema;
+	private int currentResource;
+	private int currentEmpty;
+	private int currentLuck;
+	private int currentTotLine;
 
 	public Board() {
 		populate();
@@ -12,9 +20,25 @@ public class Board implements Constantes {
 
 	private void populate() {
 		for (int i = 0; i < size; i++) {
-			mCases[i] = new Cinema();
+			mCases[i] = randomCase(i);
 		}
 	}
+	
+	private Case randomCase(int i){
+		if(i % (size/4) == 0){
+			currentScript = Constantes.BOARD_NB_SCRIPT_LINE;
+			currentCinema = Constantes.BOARD_NB_CINEMA_LINE;
+			currentResource = Constantes.BOARD_NB_RESOURCE_LINE;
+			currentEmpty = Constantes.BOARD_NB_EMPTY_TOT;
+			currentLuck = Constantes.BOARD_NB_LUCK_TOT;
+			currentTotLine = Constantes.BOARD_NB_TOT_LINE;
+			return new Cinema();
+		}
+		Math.round(Math.random() * 10 );
+
+		return new Cinema();
+	}
+	
 	
 	public Case getCaseAtIndex(int index){
 		try{
